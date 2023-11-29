760589ff81aee3d5e8131f0aad7fec1f9095cf93
seoyeon08 <silversun0808@gmail.com>
2023-11-29 22:40:03 +0900

    [Add] BattlePage.module.css

--- src/pages/BattlePage.tsx
+++ src/pages/BattlePage.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import Nav from "../components/Nav"
+import style from "../css/BattlePage.module.css"
 
 function BattlePage() {
+
   return (
     <div>
-      <h3>여기는 Battle페이지</h3>
-    </div>
-  );
+        <Nav />
+        <div className={style.box}>
+          <h3> 여기는 Battle페이지</h3>
+        </div>
+        
+    </div>    
+    );
 }
 
 export default BattlePage;
